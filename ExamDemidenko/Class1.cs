e2c39db0ba5df514936c4fcfe67c4666f4a337b2
daaaaaaamn <mishan1189@gmail.com>
2023-06-22 17:29:42 +0300

    Добавьте файлы проекта.

---
+++ ExamDemidenko/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamDemidenko
+{
+    public class Person
+    {
+        // книги данного человека
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public List<Book> Library = new List<Book>();
+        public Person(string nm, string ad)
+        {
+            Name = nm;
+            Address = ad;
+        }
+        public void Show()
+        {
+            Console.WriteLine($"Человек: имя = {Name}, адрес = {Address}");
+        }
+    }
+    //  класс книг
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public Book(string nm, string avt)
+        {
+            Title = nm;
+            Author = avt;
+        }
+        public void Show()
+        {
+            Console.WriteLine(
+                 $"Книга: название = {Title}, автор = {Author}");
+        }
+    }
+}
---
+++ ExamDemidenkoConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExamDemidenko;
+
+
+namespace ExamDemidenkoConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person ps = new Person("Иванов П.А.", "г. Москва");
+            ps.Show();
+            Book b1 = new Book("Сказки", "Пушкин А.С.");
+            ps.Library.Add(b1);
+            Book b2 = new Book("Язык C#", "Григорьев В.С.");
+            ps.Library.Add(b2);
+            foreach (Book b in ps.Library)
+            {
+                b.Show();
+            }
+            Console.ReadKey();
+        }
+    }
+
+}
